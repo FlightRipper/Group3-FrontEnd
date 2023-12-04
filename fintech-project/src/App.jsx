8a1670b036e47f549f4ccae659e6143e98350654
nancyrahhal <nancyrahhal97@gmail.com>
2023-12-04 09:18:06 +0200

    admin dashboard

--- fintech-project/src/App.jsx
+++ fintech-project/src/App.jsx
@@ -1,38 +1,36 @@
-import './App.css';
-import { BrowserRouter, Routes, Route,Outlet } from 'react-router-dom';
-import SigninPage from './pages/SignInPage'
-import RegisterPage from './pages/RegisterPage'
-import HomePage from './pages/HomePage'
-import Navbar from './components/Navbar'
-import { useState } from 'react';
-import AdminSignIn from './pages/admin/AdminSignIn';
-import AdminHome from './pages/admin/AdminHome';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import SigninPage from "./pages/SignInPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import { useState } from "react";
+import AdminSignIn from "./pages/admin/AdminSignIn";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 // import io from 'socket.io-client';
 
 // const socket = io.connect('http://localhost:5000/');
 
-
 function App() {
-
   const Layout = () => {
     return (
       <>
-      <Navbar />
-      <Outlet />
+        <Navbar />
+        <Outlet />
       </>
-    )
-  }
+    );
+  };
 
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/admin' element={<AdminSignIn />}></Route>
-      <Route path='/admin/home' element={<AdminHome />}></Route>
-        <Route path='/Register' element={<RegisterPage />}></Route>
-        <Route path='/SignIn' element={<SigninPage />}></Route>
-        <Route path='/admin' element={<HomePage />}></Route>
-        <Route path='/' element={<Layout />}>
-        <Route path='/' element={<HomePage />}></Route>
+        <Route path="/admin" element={<AdminSignIn />}></Route>
+        <Route path="/admin/home" element={<AdminDashboard />}></Route>
+        <Route path="/Register" element={<RegisterPage />}></Route>
+        <Route path="/SignIn" element={<SigninPage />}></Route>
+        <Route path="/admin" element={<HomePage />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<HomePage />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
